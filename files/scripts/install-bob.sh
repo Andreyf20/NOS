f93acde66acc37dab4542cfa43978c284d85b9d6
Andrey Torres <atorresf20@gmail.com>
2026-08-18 22:48:13 -0600

    Fix wrong dir in bob-nvim script

--- files/scripts/install-bob.sh
+++ files/scripts/install-bob.sh
@@ -4,6 +4,6 @@ set -ouex pipefail
 echo "Installing bob-nvim..."
 curl -Lo /tmp/bob.zip "https://github.com/MordechaiHadad/bob/releases/latest/download/bob-linux-x86_64.zip"
 unzip /tmp/bob.zip -d /tmp/
-mv /tmp/bob /usr/bin/bob
+mv /tmp/bob-linux-x86_64/bob /usr/bin/bob
 chmod +x /usr/bin/bob
 rm -f /tmp/bob.zip
