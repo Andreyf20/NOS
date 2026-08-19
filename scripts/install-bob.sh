b49badf06dc6e227399563eae4c579589aa81286
Andrey Torres <atorresf20@gmail.com>
2026-08-18 20:51:43 -0600

    Add initial setup packages

---
+++ scripts/install-bob.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -ouex pipefail
+
+echo "Installing bob-nvim..."
+curl -Lo /tmp/bob.zip "https://github.com/MordechaiHadad/bob/releases/latest/download/bob-linux-x86_64.zip"
+unzip /tmp/bob.zip -d /tmp/
+mv /tmp/bob /usr/bin/bob
+chmod +x /usr/bin/bob
+rm -f /tmp/bob.zip
